c0b26d1200953b8a39447cdf0bb1f7b6ef9b5217
jaro.jozefiak <jaro.jozefiak@student.po.edu.pl>
2019-10-02 12:24:37 +0200

    boolean check

--- scripts/add-content.js
+++ scripts/add-content.js
@@ -30,7 +30,17 @@ var message;
 username = 'Jarek';
 message = 'Zapraszam do zapoznania się z moim portfolio';
 
-var elName = document.getElementById('name');
+/*var elName = document.getElementById('name');
 elName.textContent = username;
 var elNote = document.getElementById('note');
-elNote.textContent = message;
+elNote.textContent = message;*/
+
+var inStock;
+var shipping;
+inStock = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStock;
+var elShip = document.getElementById('shipping');
+elShip.className = shipping;
